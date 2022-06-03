38f6818e776439c52de80d8f017cde2051c9d9f5
Digitelsystems <103522787+Digitelsystems@users.noreply.github.com>
2022-06-03 02:12:04 -0700

    Update ubuntu_openconnectws.sh

--- ubuntu_openconnectws.sh
+++ ubuntu_openconnectws.sh
@@ -2,10 +2,10 @@
 cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
-db_host='173.82.65.242';
-db_user='vpnmazasif_vpnmazasif';
-db_pass='digi#0550013763';
-db_name='vpnmazasif_vpnmazasif';
+db_host='66.45.248.110';
+db_user='digitelp_vpnmazasif';
+db_pass='ME2HE2Di@';
+db_name='digitelp_vpnmazasif';
 
 install_require()
 {
